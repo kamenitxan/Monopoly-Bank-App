63ffdc8e6f49f55f7ac901f57bad1571f4e1a4ca
kamenitxan <tomaspavel@TPs-MacBook-Pro.local>
2013-08-26 23:05:07 +0200

    vypis penez hrace

--- core/core.php
+++ core/core.php
@@ -49,5 +49,31 @@ function seznamhracu() {
         echo 'ERROR: ' . $e->getMessage();
     }
 }
+
+function penizehrace($jmeno) {
+    try {
+        $conn = new PDO('sqlite:core/Bank.db');
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+     
+        $stmt = $conn->prepare('SELECT penize FROM ucty WHERE jmeno = :jmeno');
+        $stmt->execute(array('jmeno' => $jmeno));
+ 
+        # Get array containing all of the result rows
+        $result = $stmt->fetchAll(); 
+        # If one or more rows were returned...
+        if ( count($result) ) {
+            foreach($result as $row) {
+                echo "Hodnota na uctu: " . $row[0];
+            }
+        } else {
+            echo "Hrac " . $jmeno . " nenalezen";
+        }
+    
+    } catch(PDOException $e) {
+        echo 'ERROR: ' . $e->getMessage();
+    }
+
+}
+
 ?>
 
